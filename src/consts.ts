86499c0f506e7cbc8494e90088b05e0b4193a40e
Aadhavann <aadh.ponniah@gmail.com>
2025-10-01 20:40:31 +0530

    Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Prompt & Circumstance';
+export const SITE_DESCRIPTION = 'Exploring LLMs, Machine Learning, Data Science, and Finance - by Aadhavan';
